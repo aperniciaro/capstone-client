e464917d55bc2500ab30909aae5f733f718740c7
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-04-01 16:25:56 -0400

    default value for roster is custom

--- Models/Roster.cs
+++ Models/Roster.cs
@@ -6,7 +6,7 @@ namespace capstone_client.Models
   {
     public int Id { get; set; }
     public string Name { get; set; }
-    public bool IsCustom { get; set; }
+    public bool IsCustom { get; set; } = false;
     public int ProjectedWins { get; set; }
     public Team Team { get; set; }
     public List<Player> Players { get; set; } = new List<Player>();
