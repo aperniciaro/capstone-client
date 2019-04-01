5306c016772bdfafe5f3fca01deed96276d03de0
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-04-01 16:09:31 -0400

    changed color props to arrays

--- Models/Team.cs
+++ Models/Team.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace capstone_client.Models
@@ -7,9 +8,9 @@ namespace capstone_client.Models
     public int Id { get; set; }
     public string Name { get; set; }
     public string Location { get; set; }
-    public string PrimaryColor { get; set; }
-    public string SecondaryColor { get; set; }
-    public string TertiaryColor { get; set; }
+    public int[] PrimaryColor { get; set; } = new int[3];
+    public int[] SecondaryColor { get; set; } = new int[3];
+    public int[] TertiaryColor { get; set; } = new int[3];
     public List<Roster> Rosters { get; set; } = new List<Roster>();
   }
 }
