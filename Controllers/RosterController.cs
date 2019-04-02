0a3e1dc922bccf73fd809fe9bb32b600d7474a69
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-04-02 10:41:05 -0400

    set up controllers

--- Controllers/RosterController.cs
+++ Controllers/RosterController.cs
@@ -1,7 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using capstone_client.Models;
+using content;
+
 namespace capstone_client.Controllers
 {
-    public class RosterController
+  [Route("api/[controller]")]
+  [ApiController]
+  public class RosterController : ControllerBase
+  {
+    private DatabaseContext db;
+    public RosterController()
     {
-        
+      this.db = new DatabaseContext();
     }
+
+
+  }
 }
--- Controllers/TeamController.cs
+++ Controllers/TeamController.cs
@@ -1,7 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using capstone_client.Models;
+using content;
+
 namespace capstone_client.Controllers
 {
-    public class TeamController
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TeamController : ControllerBase
+  {
+    private DatabaseContext db;
+    public TeamController()
     {
-        
+      this.db = new DatabaseContext();
     }
+
+
+  }
 }
