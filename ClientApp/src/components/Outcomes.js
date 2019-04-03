85e1acc7a1c8e081d2ce89c8aae2ea7acf992102
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-04-03 14:59:30 -0400

    removed team stats

--- ClientApp/src/components/Outcomes.js
+++ ClientApp/src/components/Outcomes.js
@@ -4,24 +4,17 @@ class Outcomes extends Component {
   render() {
     return (
       <section className="outcomes">
-        <section className="expectations">
-          <section className="expected-record">
-            <h3>Expected Record: </h3>
-            <p>82-80</p>
-          </section>
+        <section className="expected-record">
+          <h3>Expected Record: </h3>
+          <p>82-80</p>
+        </section>
+        <section>
           <h3>Projected Division Standings: </h3>
           <ol className="division-standings">
             {this.props.divisionTeams.map(team => {
               return <li key={team.mlb_org_id}>{team.name_display_full}</li>
             })}
           </ol>
-        </section>
-        <section className="team-stats">
-          <h3>Projected Team Stats: </h3>
-          <p>HRs: </p>
-          <p>SBs: </p>
-          <p>BA: </p>
-          <p>ERA: </p>
         </section>
       </section>
     )
