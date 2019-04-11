e0decc7d3318bf8d7cbdd88dffdf285242973390
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-04-11 11:13:13 -0400

    fixed li alignment

--- ClientApp/src/components/FungibleRoster.js
+++ ClientApp/src/components/FungibleRoster.js
@@ -10,12 +10,12 @@ class FungibleRoster extends Component {
           .map(player => {
             return (
               <li className="movable-player" key={player.id}>
-                <Link to={`/Player/${player.mlbId}`}>
-                  <h3>{player.playerName}</h3>
-                </Link>
                 <button onClick={() => this.props.movePlayer(player.id)}>
                   +
                 </button>
+                <Link to={`/Player/${player.mlbId}`}>
+                  <h3>{player.playerName}</h3>
+                </Link>
               </li>
             )
           })}
